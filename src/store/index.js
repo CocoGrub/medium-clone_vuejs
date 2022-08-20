8a71a94cec4b11f8158c62920ad8a5e7fff337c9
Tedd <fedsavin@gmail.com>
2022-08-21 00:56:30 +0300

    added vuex example

--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,13 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {counter: 0},
   getters: {},
-  mutations: {},
+  mutations: {
+    incrementCounter() {
+      this.state.counter += 1
+    },
+  },
   actions: {},
   modules: {},
 })
---
+++ src/views/Register.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Sign up</h1>
+          <p class="text-xs-center">
+            <router-link :to="{name: 'login'}"> Need an account? </router-link>
+          </p>
+          VALIDATION ERRORS
+          <form @submit.prevent="onSubmit">
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="text"
+                placeholder="Username"
+                v-model="username"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="text"
+                placeholder="Email"
+                v-model="email"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="password"
+                placeholder="Password"
+                v-model="password"
+              />
+            </fieldset>
+            <button
+              class="btn btn-lg btn-primary pull-xs-right"
+              :disabled="isSubmitting"
+            >
+              Sign Up
+            </button>
+          </form>
+          <button @click="increaseCounter">click</button>
+          <div>{{ getCount }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'McvRegister',
+  methods: {
+    increaseCounter() {
+      this.$store.commit('incrementCounter')
+    },
+  },
+  computed: {
+    getCount() {
+      return this.$store.state.counter
+    },
+  },
+}
+</script>
